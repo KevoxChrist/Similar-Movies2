61c6fb53e35a995b748a268007714596b1576d18
Kevin Anderson <Kevin.anderson35@yahoo.com>
2025-09-12 16:58:48 -0400

    first commit

---
+++ client/index.js
@@ -0,0 +1,71 @@
+const form = document.getElementById("search-form");
+const input = document.getElementById("search-input");
+const resultsDiv = document.getElementById("results");
+const similarBtn = document.querySelectorAll(".similar-btn")
+
+form.addEventListener("input", async (e) => {
+  e.preventDefault(); // stop page reload
+  const query = input.value;
+
+  const response = await fetch(`/search?query=${encodeURIComponent(query)}`);
+  const data = await response.json();
+
+
+  const movieCard = data.results
+    .map(
+      (movie) => `
+        <div class="movie-card" id=${movie.id}>
+          <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" 
+               alt="${movie.title} Poster">
+            <div class = "movie-desc">
+               <p >${movie.title} (${movie.release_date})</p>
+           <button class="similar-btn">Similar</button>
+        </div>
+
+        
+          <div class="similar-content"></div>
+      
+           </div>
+      `
+    )
+    .join("");
+
+
+//// <button class="similar-btn">Similar</button>
+
+  resultsDiv.innerHTML = movieCard 
+});
+
+
+
+
+resultsDiv.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("similar-btn")) {
+    e.stopPropagation();
+
+    const card = e.target.closest(".movie-card"); //This will get the closet movie card which would be the parent div
+    const movieId = card.id; //movie card div's id
+
+   
+
+
+
+
+    console.log("Movie ID:", movieId);
+
+    const response = await fetch(`/similar?movie_id=${movieId}`);
+    const data = await response.json();
+
+    
+    card.innerHTML +=data.results
+    .map(
+      (movie) => `<p> ${movie.title}</p>`
+        
+    )
+    .join("");
+
+    console.log("Similar movies:", data);
+    
+  }
+});
+
---
+++ web-server/src/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+require("dotenv").config();
+const request = require("postman-request");
+const path = require("path");
+
+
+
+const app = express();
+const apiKey = process.env.TMDB_API_KEY;
+const PORT = 3000;
+
+console.log(apiKey);
+
+
+app.use(express.static(path.join(__dirname, "../../client")))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/index.html"));
+});
+
+
+
+// Search route
+app.get("/search", (req, res) => {
+  const query = req.query.query;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+
+  request({ url: url, json: true }, (err, response, body) => {
+    if (err) return res.status(500).json({ error: "Request failed" });
+    res.send(body);
+  });
+});
+
+app.get("/similar", (req, res) => {
+  const movieId = req.query.movie_id; 
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${apiKey}`;
+
+  request({ url, json: true }, (err, response, body) => {
+    if (err) return res.status(500).json({ error: "Request failed" });
+    res.send(body);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+
+
